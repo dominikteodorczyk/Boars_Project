5f13d0ab7fef484c8ea75b9241dda41aa164ac2b
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2025-01-03 15:12:52 +0100

    black reformatted;

--- laws_params.py
+++ laws_params.py
@@ -9,12 +9,11 @@ import traceback
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-    )
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-INFOSTOP_DATA_DIR = ''
-OUTPUT_DIR = 'scaling_laws_output'
+INFOSTOP_DATA_DIR = ""
+OUTPUT_DIR = "scaling_laws_output"
 
 
 def main():
@@ -25,8 +24,7 @@ def main():
 
         # Create output directory for results
         output_dir = create_output_directory(
-            base_path=os.path.dirname(__file__),
-            dir_name=OUTPUT_DIR
+            base_path=os.path.dirname(__file__), dir_name=OUTPUT_DIR
         )
 
         dataset_stats = DataSetStats(output_dir)
@@ -39,19 +37,20 @@ def main():
                 clean_data = DataIO.open_for_scaling_laws(parsed_file)
                 data_name = DataIO.get_animal_name(parsed_file)
 
-                sl_calc = ScalingLawsCalc(clean_data, data_name, output_dir, dataset_stats)
+                sl_calc = ScalingLawsCalc(
+                    clean_data, data_name, output_dir, dataset_stats
+                )
                 sl_calc.process_file()
             except Exception as e:
-                logging.warning(f'There is problem with {os.path.basename(parsed_file)}: {e}')
+                logging.warning(
+                    f"There is problem with {os.path.basename(parsed_file)}: {e}"
+                )
 
-        dataset_stats.stats_set.to_csv('animal_stats.csv')
+        dataset_stats.stats_set.to_csv("animal_stats.csv")
 
     except Exception as e:
-        logging.error(
-            f"An unexpected error occurred in the main workflow: {e}"
-        )
+        logging.error(f"An unexpected error occurred in the main workflow: {e}")
         traceback.print_exc()
-
 
 
 if __name__ == "__main__":
